92c4853d223544a271475dfec65a09943172e1db
DESKTOP-9B8C0QK\IT1410004 <pyl1279@naver.com>
2018-11-05 16:54:29 +0900

    세팅 수정

--- vue-front/src/views/Settings.vue
+++ vue-front/src/views/Settings.vue
@@ -15,14 +15,21 @@
 
             <div class="content">
                 <div class="card">
-                    <div class="card-body">
-                        <div class="toolbar">
-                            <!--        Here you can write extra buttons/actions for the toolbar              -->
+                    <div class="card-header">
+                        <div class="row">
+                            <h4 class="col-sm-11">
+                                Settings
+                            </h4>
+                            <div class="col-sm-1">
+                                <button style="float:right;" class="btn btn-primary btn-sm tim-icons icon-simple-add" onclick="showSwal('input-field')"></button>
+                            </div>
                         </div>
+                    </div>
+                    <div class="card-body">
                         <div id="datatable_wrapper" class="dataTables_wrapper dt-bootstrap4">
                             <div class="row">
                                 <div class="col-sm-12 col-md-6">
-                                    <div class="dataTables_length" id="datatable_length">
+                                    <div class="dataTables_length" id="datatable_length1">
                                         <label>Show
                                             <select name="datatable_length" aria-controls="datatable" class="custom-select custom-select-sm form-control form-control-sm">
                                                 <option value="10">10</option>
@@ -46,122 +53,23 @@
                                     <table id="datatable" class="table table-striped dataTable dtr-inline collapsed" role="grid" aria-describedby="datatable_info" style="width: 1091px;">
                                         <thead>
                                             <tr role="row">
-                                                <th class="sorting_asc" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" style="width: 50px;" aria-sort="ascending" >Name</th>
-                                                <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" style="width: 15px;">Position</th>
-                                                <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" style="width: 153px;" >Office</th>
-                                                <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" style="width: 69px;" >Age</th>
-                                                <th class="" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" style="width: 153px; display: none;">Actions</th>
+                                                <th class="sorting_asc text-center header" tabindex="0" aria-controls="datatable" style="width: 50px;" >타입</th>
+                                                <th class="sorting text-center header" tabindex="0" aria-controls="datatable" style="width: 50px;">구분</th>
+                                                <th class="sorting text-center header" tabindex="0" aria-controls="datatable"  >데이터1</th>
+                                                <th class="sorting text-center header" tabindex="0" aria-controls="datatable"  >데이터2</th>
+                                                <th class="sorting text-center header" tabindex="0" aria-controls="datatable">데이터3</th>
+                                                <th class="sorting text-center header" tabindex="0" aria-controls="datatable"  style="width: 20px;">&nbsp;</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr role="row" class="odd">
-                                                <td tabindex="0" class="sorting_1">Airi Satou</td>
-                                                <td>Accountant</td>
-                                                <td>Tokyo</td>
-                                                <td>33</td>
-                                                <td class="text-right" style="display: none;">
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-info btn-icon btn-sm btn-neutral like"><i class="tim-icons icon-heart-2"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-warning btn-icon btn-sm btn-neutral  edit"><i class="tim-icons icon-pencil"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-danger btn-icon btn-sm btn-neutral remove"><i class="tim-icons icon-simple-remove"></i></a>
-                                                </td>
-                                            </tr>
-                                            <tr role="row" class="even">
-                                                <td class="sorting_1" tabindex="0">Angelica Ramos</td>
-                                                <td>Chief Executive Officer (CEO)</td>
-                                                <td>London</td>
-                                                <td>47</td>
-                                                <td class="text-right" style="display: none;">
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-info btn-icon btn-sm btn-neutral like"><i class="tim-icons icon-heart-2"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-warning btn-icon btn-sm btn-neutral  edit"><i class="tim-icons icon-pencil"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-danger btn-icon btn-sm btn-neutral remove"><i class="tim-icons icon-simple-remove"></i></a>
-                                                </td>
-                                            </tr>
-                                            <tr role="row" class="odd">
-                                                <td tabindex="0" class="sorting_1">Ashton Cox</td>
-                                                <td>Junior Technical Author</td>
-                                                <td>San Francisco</td>
-                                                <td>66</td>
-                                                <td class="text-right" style="display: none;">
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-info btn-icon btn-sm btn-neutral like"><i class="tim-icons icon-heart-2"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-warning btn-icon btn-sm btn-neutral  edit"><i class="tim-icons icon-pencil"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-danger btn-icon btn-sm btn-neutral remove"><i class="tim-icons icon-simple-remove"></i></a>
-                                                </td>
-                                            </tr>
-                                            <tr role="row" class="even">
-                                                <td class="sorting_1" tabindex="0">Bradley Greer</td>
-                                                <td>Software Engineer</td>
-                                                <td>London</td>
-                                                <td>41</td>
-                                                <td class="text-right" style="display: none;">
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-info btn-icon btn-sm btn-neutral like"><i class="tim-icons icon-heart-2"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-warning btn-icon btn-sm btn-neutral  edit"><i class="tim-icons icon-pencil"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-danger btn-icon btn-sm btn-neutral remove"><i class="tim-icons icon-simple-remove"></i></a>
-                                                </td>
-                                            </tr>
-                                            <tr role="row" class="odd">
-                                                <td class="sorting_1" tabindex="0">Brenden Wagner</td>
-                                                <td>Software Engineer</td>
-                                                <td>San Francisco</td>
-                                                <td>28</td>
-                                                <td class="text-right" style="display: none;">
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-info btn-icon btn-sm btn-neutral like"><i class="tim-icons icon-heart-2"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-warning btn-icon btn-sm btn-neutral  edit"><i class="tim-icons icon-pencil"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-danger btn-icon btn-sm btn-neutral remove"><i class="tim-icons icon-simple-remove"></i></a>
-                                                </td>
-                                            </tr>
-                                            <tr role="row" class="even">
-                                                <td tabindex="0" class="sorting_1">Brielle Williamson</td>
-                                                <td>Integration Specialist</td>
-                                                <td>New York</td>
-                                                <td>61</td>
-                                                <td class="text-right" style="display: none;">
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-info btn-icon btn-sm like"><i class="tim-icons icon-heart-2"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-warning btn-icon btn-sm edit"><i class="tim-icons icon-pencil"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-danger btn-icon btn-sm remove"><i class="tim-icons icon-simple-remove"></i></a>
-                                                </td>
-                                            </tr>
-                                            <tr role="row" class="odd">
-                                                <td class="sorting_1" tabindex="0">Caesar Vance</td>
-                                                <td>Pre-Sales Support</td>
-                                                <td>New York</td>
-                                                <td>21</td>
-                                                <td class="text-right" style="display: none;">
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-info btn-icon btn-sm like"><i class="tim-icons icon-heart-2"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-warning btn-icon btn-sm edit"><i class="tim-icons icon-pencil"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-danger btn-icon btn-sm remove"><i class="tim-icons icon-simple-remove"></i></a>
-                                                </td>
-                                            </tr>
-                                            <tr role="row" class="even">
-                                                <td tabindex="0" class="sorting_1">Cedric Kelly</td>
-                                                <td>Senior Javascript Developer</td>
-                                                <td>Edinburgh</td>
-                                                <td>22</td>
-                                                <td class="text-right" style="display: none;">
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-info btn-icon btn-sm like"><i class="tim-icons icon-heart-2"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-warning btn-icon btn-sm edit"><i class="tim-icons icon-pencil"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-danger btn-icon btn-sm remove"><i class="tim-icons icon-simple-remove"></i></a>
-                                                </td>
-                                            </tr>
-                                            <tr role="row" class="odd">
-                                                <td class="sorting_1" tabindex="0">Charde Marshall</td>
-                                                <td>Regional Director</td>
-                                                <td>San Francisco</td>
-                                                <td>36</td>
-                                                <td class="text-right" style="display: none;">
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-info btn-icon btn-sm like"><i class="tim-icons icon-heart-2"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-warning btn-icon btn-sm edit"><i class="tim-icons icon-pencil"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-danger btn-icon btn-sm remove"><i class="tim-icons icon-simple-remove"></i></a>
-                                                </td>
-                                            </tr>
-                                            <tr role="row" class="even">
-                                                <td tabindex="0" class="sorting_1">Colleen Hurst</td>
-                                                <td>Javascript Developer</td>
-                                                <td>San Francisco</td>
-                                                <td>39</td>
-                                                <td class="text-right" style="display: none;">
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-info btn-icon btn-sm like"><i class="tim-icons icon-heart-2"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-warning btn-icon btn-sm edit"><i class="tim-icons icon-pencil"></i></a>
-                                                    <a href="javascript:void(0)" class="btn btn-link btn-danger btn-icon btn-sm remove"><i class="tim-icons icon-simple-remove"></i></a>
+                                            <tr >
+                                                <td tabindex="0" class="sorting_1 text-center">0000</td>
+                                                <td class="text-center">0000</td>
+                                                <td class="text-center">Tokyo</td>
+                                                <td class="text-center">33</td>
+                                                <td class="text-center"></td>
+                                                <td class="text-center">
+                                                    <i class="tim-icons icon-simple-remove"></i>
                                                 </td>
                                             </tr>
                                         </tbody>
