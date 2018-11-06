4af2e3deef3083a5f3607d76cb7ce95808b3f5d1
DESKTOP-9B8C0QK\IT1410004 <pyl1279@naver.com>
2018-11-06 10:03:36 +0900

    모달뷰 추가

---
+++ vue-front/src/components/settings/modal.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="modal fade" id="settingsModal" tabindex="-1" role="dialog" aria-labelledby="settingsModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="settingsModalLabel">Settings 입력</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                        <i class="tim-icons icon-simple-remove"></i>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "modal"
+    }
+</script>
+
+<style scoped>
+
+</style>
