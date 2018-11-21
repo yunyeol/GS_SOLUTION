730407f081b80f780f88d4f860ecd919274ddba9
foodbin919 <foodbin919@naver.com>
2018-11-21 17:03:32 +0900

    수신함 작업 진행.

--- alarme-web/src/main/java/com/project/alarmeweb/controller/mail/ReceiverController.java
+++ alarme-web/src/main/java/com/project/alarmeweb/controller/mail/ReceiverController.java
@@ -1,18 +1,35 @@
 package com.project.alarmeweb.controller.mail;
 
 import com.project.alarmeweb.controller.BaseController;
+import com.project.alarmeweb.dto.Group;
+import com.project.alarmeweb.service.GroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Locale;
+import org.springframework.web.servlet.ModelAndView;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ReceiverController extends BaseController {
 
-    @GetMapping(value={"/mail/receiver"}, produces="text/html; charset=UTF-8")
-    public String campaign(Locale locale, Model model){
+    @Autowired
+    private GroupService groupService;
 
-        return "mail/receiver/receiver";
+    @GetMapping(value={"/mail/receiver"}, produces="text/html; charset=UTF-8")
+    public ModelAndView receiver(){
+        return new ModelAndView("mail/receiver/receiver");
+    }
+
+    @GetMapping(value={"/mail/receiver/group"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity group(){
+        Map resultData = new HashMap();
+        List<Group> groupList = groupService.getGroupList();
+        resultData.put("data", (groupList != null) ? groupList : "[]"  );
+        return new ResponseEntity(resultData, HttpStatus.OK);
     }
 }
