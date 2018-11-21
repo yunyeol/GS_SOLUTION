30f26f07d3b6000c36ef5703fe86c2e69867c507
DESKTOP-9B8C0QK\IT1410004 <pyl1279@naver.com>
2018-11-21 17:58:11 +0900

    ck에디터 설정 추가

--- alarme-web/src/main/resources/static/ckeditor/config.js
+++ alarme-web/src/main/resources/static/ckeditor/config.js
@@ -7,4 +7,18 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+
+    config.toolbar = [
+        { name: 'document', items: [ 'Source' ] },
+        { name: 'styles', items: [ 'Format', 'Font', 'FontSize' ] },
+        { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike'] },
+        { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+        { name: 'align', items: [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
+        { name: 'links', items: [ 'Link', 'Unlink' ] },
+        { name: 'insert', items: [ 'Image', 'Table' ] },
+        { name: 'Mapping', items:['LoginId', 'UserName', 'Title', 'Contents']}
+    ],
+    config.customConfig = '',
+    config.height = 400
+    config.resize = 0
 };
--- alarme-web/src/main/resources/static/viewJs/mail/send/realtime/realtime.js
+++ alarme-web/src/main/resources/static/viewJs/mail/send/realtime/realtime.js
@@ -2,20 +2,7 @@
 
     realtime.init = function(){
 
-        CKEDITOR.replace( 'editor', {
-            toolbar: [
-                { name: 'document', items: [ 'Source' ] },
-                { name: 'styles', items: [ 'Format', 'Font', 'FontSize' ] },
-                { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike'] },
-                { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
-                { name: 'align', items: [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
-                { name: 'links', items: [ 'Link', 'Unlink' ] },
-                { name: 'insert', items: [ 'Image', 'Table' ] },
-                { name: 'Mapping', items:['LoginId', 'UserName', 'Title', 'Contents']}
-            ],
-            customConfig: '',
-            height: 400,
-        });
+        CKEDITOR.replace( 'editor');
 
 
         this.setEvent();
