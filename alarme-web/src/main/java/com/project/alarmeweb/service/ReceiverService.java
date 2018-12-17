fb03d38b1884742e43a3f902631bbafaa51d1192
foodbin919 <foodbin919@naver.com>
2018-12-17 15:45:57 +0900

    DateUtil add, 그룹대상관리 팝업 진행..

--- alarme-web/src/main/java/com/project/alarmeweb/service/ReceiverService.java
+++ alarme-web/src/main/java/com/project/alarmeweb/service/ReceiverService.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public interface ReceiverService {
     List<Receiver> getReceiverList();
-    int getReceivGrpNameCnt(Map<String,Object> params);
+    int getReceivGrpNameCnt(Map<String,String> params);
     int addReceiver(Receiver receiver);
     int modifyReceiver(Receiver receiver);
     int removeReceiver(Long addrGrpId);
