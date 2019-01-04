d5f3e68ca94361595943b71d2a404bf276c113df
DESKTOP-9B8C0QK\IT1410004 <pyl1279@naver.com>
2019-01-04 17:28:14 +0900

    결과개발중

--- mail-engine/src/main/java/gs/mail/engine/util/SmtpSocket.java
+++ mail-engine/src/main/java/gs/mail/engine/util/SmtpSocket.java
@@ -45,19 +45,19 @@ public class SmtpSocket {
                 File file = new File(fileDir + "sendLog_"+send.getSchdlId()+"_" + sdf.format(today) + ".log");
                 sendLog = new PrintWriter(new BufferedWriter(new FileWriter(file, true)), true);
 
-                sendLog.print("SOCKET_CONN:"+br.readLine()+"||");
+                sendLog.print("SOCKET_CONN:"+subStringResult(br.readLine())+"||");
                 sendLog.print("UUID:"+send.getUuid()+"||");
                 ps.print("HELO "+send.getReceiver().substring(send.getReceiver().indexOf("@")+1)+carriageReturn );
-                sendLog.print("HELO_RES:"+br.readLine()+"||");
+                sendLog.print("HELO_RES:"+subStringResult(br.readLine())+"||");
 
                 ps.print("MAIL FROM: <"+send.getSender()+">"+carriageReturn);
-                sendLog.print("MAIL_FROM_RES:"+br.readLine()+"||");
+                sendLog.print("MAIL_FROM_RES:"+subStringResult(br.readLine())+"||");
 
                 ps.print("RCPT TO:<"+send.getReceiver()+">"+carriageReturn);
-                sendLog.print("RCPT_TO_RES:"+br.readLine()+"||");
+                sendLog.print("RCPT_TO_RES:"+subStringResult(br.readLine())+"||");
 
                 ps.print("DATA"+carriageReturn);
-                sendLog.print("DATA_RES:"+br.readLine()+"||");
+                sendLog.print("DATA_RES:"+subStringResult(br.readLine())+"||");
                 ps.print("Mime-Version: 1.0"+carriageReturn);
                 ps.print("Content-Type:text/html;charset=euc-kr"+carriageReturn);
                 ps.print("Content-Transfer-Encoding:8bit"+carriageReturn);
@@ -71,8 +71,8 @@ public class SmtpSocket {
                 ps.print("QUIT"+carriageReturn);
                 ps.flush();
 
-                sendLog.print("DATA_SEND_RES:"+br.readLine()+"||");
-                sendLog.print("SEND_RES:"+br.readLine()+"||");
+                sendLog.print("DATA_SEND_RES:"+subStringResult(br.readLine())+"||");
+                sendLog.print("SEND_RES:"+subStringResult(br.readLine())+"||");
                 sendLog.print("QUIT");
                 sendLog.println();
                 return br.readLine();
@@ -115,6 +115,15 @@ public class SmtpSocket {
         }catch (Exception e){
             e.printStackTrace();
             return null;
+        }
+    }
+
+    public String subStringResult(String rs){
+        int length = rs.length();
+        if(length > 6){
+            return rs.substring(0,9);
+        }else{
+            return rs.substring(0,3);
         }
     }
 }
