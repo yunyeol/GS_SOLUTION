22bb1bd97aa4bad2c299aaf09d61c527319040e9
foodbin919 <foodbin919@naver.com>
2019-02-25 22:00:19 +0900

    프로필 (업데이트항목 확인중..)

--- alarme-web/src/main/java/com/project/alarmeweb/controller/UserController.java
+++ alarme-web/src/main/java/com/project/alarmeweb/controller/UserController.java
@@ -1,19 +1,29 @@
 package com.project.alarmeweb.controller;
 
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.project.alarmeweb.dto.User;
+import com.project.alarmeweb.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
 import java.util.Locale;
 
 @Controller
 public class UserController extends  BaseController{
 
+    @Autowired
+    UserService userService;
+
     @GetMapping(value={"/user/profile"}, produces="text/html; charset=UTF-8")
     public String login(Locale locale, Model model){
+        List<User> userList = userService.getMemberList(loginId(), null);
+        model.addAttribute("memberList",userList);
         return "user/profile";
     }
 
